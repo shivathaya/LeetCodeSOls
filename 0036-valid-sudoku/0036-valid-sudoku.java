2af6ef2c0bfb666fa1b8a3b5c7b529240e9e0712
Abhishek Shivathaya <114136171+shivathaya@users.noreply.github.com>
2025-09-03 23:17:39 +0530

    Time: 21 ms (6.74%), Space: 46.1 MB (6.18%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,15 +1,20 @@
 class Solution {
+    // created this record for the the key of hash mao x,y rep each sub 3x3 box
     public record Pair(int x, int y) {};
     public boolean isValidSudoku(char[][] board) {
 
-        Map<Integer, Set<Character>> rowS = new HashMap<>();
-        Map<Integer, Set<Character>> colS = new HashMap<>();
-        Map<Pair, Set<Character>> squr = new HashMap<>();
-
+        Map<Integer, Set<Character>> rowS = new HashMap<>();// seen in row
+        Map<Integer, Set<Character>> colS = new HashMap<>();//seen in col
+        Map<Pair, Set<Character>> squr = new HashMap<>();//seen in 3x3 box
+        // always till 9 bec max size is 9
         for(int r = 0; r< 9; r++){
             for(int c = 0; c< 9; c++){
+                //. means not filled so skip it 
                 if(board[r][c] == '.') continue;
-
+                //if set not there for row r means create new row
+                //check if the num laready in that row-> means duplicate -> invalid
+                // else add the num to row set
+                // do same for column and 3x3 square
                 if (rowS.computeIfAbsent(r, k -> new HashSet<>()).contains(board[r][c])) return false;
                 rowS.get(r).add(board[r][c]);
                 if (colS.computeIfAbsent(c, k -> new HashSet<>()).contains(board[r][c])) return false;
