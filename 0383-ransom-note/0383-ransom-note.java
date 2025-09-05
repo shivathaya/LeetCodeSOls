a1cb33683f6474feaa39e1b7982086ac221647fb
Abhishek Shivathaya <114136171+shivathaya@users.noreply.github.com>
2025-09-05 20:49:48 +0530

    Time: 11 ms (37.07%), Space: 45.4 MB (25.19%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,13 +1,14 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
         Map<Character,Integer> mag = new HashMap<>();
-
+        //add magazine in hashmap with count
         for(char c: magazine.toCharArray()){
             mag.put(c, mag.getOrDefault(c,0)+1);
         }
-
+        //if char of s in map nad count more than 0
         for(char s: ransomNote.toCharArray()){
             if(mag.containsKey(s) && mag.get(s)>0){
+                //continue and decrement count
                 mag.put(s,mag.get(s)-1);
             }else return false;
         }
