3299b4cfa89d0eca832639ecb16fb3a6d8370a96
Abhishek Shivathaya <114136171+shivathaya@users.noreply.github.com>
2026-04-15 21:58:14 +0530

    Time: 1 ms (97.78%), Space: 42.8 MB (28.61%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int mySqrt(int x) {
+        if(x ==0 || x == 1){
+            return x;
+        }
+        int l = 1; 
+        int r = x; 
+        int ans =0;
+
+        while(l<=r){
+            int m = l+ (r-l)/2;
+
+            if(m <= x/m){
+                ans = m;
+                l = m+1;
+            }else{
+                r = m-1;
+            }
+        }
+
+        return ans;
+
+    }
+}
