ad174cca0a14650747c604e9b2a35486c250d3cf
Abhishek Shivathaya <114136171+shivathaya@users.noreply.github.com>
2026-06-01 21:56:35 +0530

    Time: 15 ms (63.07%), Space: 51.5 MB (35.75%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i=0; 
+        int j=0;
+
+        int res = 0;
+
+        while(j<s.length && i<g.length){
+            if(s[j] >= g[i]){
+                res++;
+                j++;
+                i++;
+            }else{
+                j++;
+            }
+        }
+
+        return res;
+    }
+}
